0d9bbef0f2670a2e5b0d8438c1e9506b92024ecd
Sophia Cheung <cheungsinhan@gmail.com>
2019-07-20 13:52:18 -0700

    Add game states: ready, game, and over

--- src/game.js
+++ src/game.js
@@ -10,6 +10,8 @@ export default class TurnItDown {
         this.background.src = "img/background.png";
         this.music = new Audio();
         this.music.src = "audio/sunflower.mp3";
+        this.gameover = new Image();
+        this.gameover.src = "img/gameover.png";
         this.x = 0;
         this.y = 600;
 
@@ -19,41 +21,51 @@ export default class TurnItDown {
         window.addEventListener("keyup", this.keyUp.bind(this));
         window.addEventListener("click", this.click.bind(this));
 
+        this.state = {
+            current: 0,
+            getReady: 0,
+            game: 1,
+            over: 2
+        }
+
         this.restart();
     }
     
     click(e){
-        if (!this.running) {
-            this.play();
-            this.music.play();
+        switch(this.state.current){
+            case this.state.getReady:
+                this.state.current = this.state.game;
+                this.music.play();
+                break;
+            case this.state.over:
+                console.log('clicked for state.over')
+                this.restart();
+                break;
         }
     }
+
+    key(e) {
+        if (this.state.current === this.state.game) {
+            if (e.keyCode === 38) this.player.jump();
+            if (e.keyCode === 37) this.player.left = true;
+            if (e.keyCode === 39) this.player.right = true;
+            if (e.keyCode === 32) this.player.duck();
+        }
+    }
+
+    keyUp(e) {
+        if (this.state.current === this.state.game) {
+            if (e.keyCode === 37) this.player.left = false;
+            if (e.keyCode === 39) this.player.right = false;
+        }
+    }
+    
     restart() {
-        this.running = false;
-        this.score = 0;
+        this.state.current = this.state.getReady;
         this.platform = new Platform(this.dimensions);
         this.player = new Player(this.dimensions, this.platform, this.music);
 
         this.animate();
-    }
-
-    play() {
-        this.running = true;
-        this.animate();
-    }
-
-    key(e) {
-        if (e.keyCode === 38) {
-            this.player.jump();
-        }
-        if (e.keyCode === 37) this.player.left = true;
-        if (e.keyCode === 39) this.player.right = true;
-        if (e.keyCode === 32) this.player.duck();
-    }
-
-    keyUp(e) {
-        if (e.keyCode === 37) this.player.left = false;
-        if (e.keyCode === 39) this.player.right = false;
     }
 
     animate(){
@@ -61,31 +73,34 @@ export default class TurnItDown {
         this.platform.animate(this.ctx);
         this.player.animate(this.ctx);
         if (this.gameOver()) {
-            alert("Game Over :(");
-            this.ctx.clearRect(0, 0, 480, 640);
-            this.restart();
+            this.ctx.clearRect(0, 0, this.dimensions.width, this.dimensions.height);
+            this.ctx.drawImage(this.gameover, 0, 0, 233, 111, this.dimensions.width / 4, this.dimensions.height / 3, 233, 111);
         } else {
-            if (this.running) {
+            if (this.state.current === this.state.game) {
                 this.frame = requestAnimationFrame(this.animate);
-            } 
+            } else if (this.state.current === this.state.over) {
+                this.ctx.clearRect(0, 0, 480, 640);
+                this.ctx.drawImage(this.gameover, 0, 0, 233, 111, this.dimensions.width / 4, this.dimensions.height / 3, 233, 111);
+                this.frame = requestAnimationFrame(this.animate);
+            } else if (this.state.current === this.state.getReady) {
+                this.ctx.clearRect(0, 0, 480, 640);
+                this.ctx.drawImage(this.background, 0, 0, 233, 111, this.dimensions.width / 4, this.dimensions.height / 3, 233, 111);
+                this.frame = requestAnimationFrame(this.animate);
+            }
         }    
     }
-
-    // stopAnimation(){
-    //     cancelAnimationFrame(this.frame);
-    // }
-
+        
     backgroundDraw(){
         this.ctx.drawImage(this.background, this.x, this.y, 480, 640, 0, 0, 480, 640);
-        if (this.y < 0) {
-            this.y += (2400-640);
-        }
+        if (this.y < 0) this.y += (2400-640);
+
         //background scroll
         this.y -= 0.2;
     }
 
     gameOver(){
         if (this.player.y >= this.dimensions.height) {
+            this.state.current = this.state.over;
             this.music.pause();
             this.music.currentTime = 0;
             return true;
---
+++ src/song.js
@@ -0,0 +1,4 @@
+// var audioCtx = new (window.audioCtx || window.webkitAudioContext)();
+// var analyser = audioCtx.createAnalyser();
+
+// window.audioCtx
